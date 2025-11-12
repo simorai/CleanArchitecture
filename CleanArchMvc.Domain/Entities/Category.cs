472944b88a5e45bdff5e2c7fc4431ea90c50aa3b
simorai <109289730+simorai@users.noreply.github.com>
2025-11-12 22:54:08 +0000

    Add domain model with entities and validation logic
    Introduced foundational domain model for Clean Architecture:
    - Added `Category` and `Product` entities with validation logic.
    - Implemented `Entity` base class for shared entity structure.
    - Created `DomainExceptionValidation` for centralized validation.
    - Updated `CleanArchMvc.Domain.csproj` to include `Interfaces` folder.
    
    These changes ensure data integrity and improve maintainability.

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Category : Entity
+    {
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id. Id must be greater than zero.");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateDomain(name);
+        }
+
+
+        public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            Domain.Validation.DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required");
+            Domain.Validation.DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+            Name = name;
+        }
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,55 @@
+﻿namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Product : Entity
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public Product(int id, string name, string description, decimal price, int stock, string image)
+        {
+            Domain.Validation.DomainExceptionValidation.When(id < 0, "Invalid Id. Id must be greater than zero.");
+            Id = id;
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            Domain.Validation.DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required");
+            Domain.Validation.DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+            Domain.Validation.DomainExceptionValidation.When(string.IsNullOrEmpty(description),
+                "Invalid description. Description is required");
+            Domain.Validation.DomainExceptionValidation.When(description.Length < 5,
+                "Invalid description, too short, minimum 5 characters");
+            Domain.Validation.DomainExceptionValidation.When(price < 0,
+                "Invalid price value");
+            Domain.Validation.DomainExceptionValidation.When(stock < 0,
+                "Invalid stock value");
+            Domain.Validation.DomainExceptionValidation.When(image?.Length > 250,
+                "Invalid image name, too long, maximum 250 characters");
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
