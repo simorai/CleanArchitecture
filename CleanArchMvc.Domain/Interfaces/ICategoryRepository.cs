42d22f276260807def4ddee1c0ed8a4a0bb0f4c2
simorai <109289730+simorai@users.noreply.github.com>
2025-11-12 23:37:22 +0000

    Add repository interfaces and generic CRUD interface
    Added `ICategoryRepository` interface for category-specific operations, extending the new generic `IGenericInterface<Category>`.
    Introduced `IGenericInterface<T>` to define common CRUD operations for entities inheriting from `Entity`.
    Added `IProductRepository` interface for product-specific operations, including methods to retrieve products and filter by category.

---
+++ CleanArchMvc.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface ICategoryRepository : IGenericInterface<Category>
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+
+    }
+}
---
+++ CleanArchMvc.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    public interface IProductRepository : IGenericInterface<Product>
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<IEnumerable<Product>> GetProductsByCategoryAsync(int? categoryId);
+    }
+}
