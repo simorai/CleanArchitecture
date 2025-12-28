9553b87450ac02bc4a7d049fce00c7e1c7051748
simorai <109289730+simorai@users.noreply.github.com>
2025-12-28 23:23:48 +0000

    Add TokenController for user authentication
    Introduced a new TokenController to handle user authentication.
    Injected the IAuthenticate dependency to manage authentication
    logic. Added a LoginUser endpoint (POST api/token/loginUser)
    to authenticate users based on their email and password.

---
+++ CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchMvc.API.Models;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authentication;
+
+        public TokenController(IAuthenticate authentication)
+        {
+            _authentication = authentication ?? throw new ArgumentNullException(nameof(authentication));
+        }
+
+        [HttpPost("loginUser")]
+        public async Task<ActionResult> LoginUser([FromBody] LoginModel userInfo)
+        {
+            var result = await _authentication.Authenticate(userInfo.Email, userInfo.Password);
+            if (result)
+            {
+                //GenerateToken(userInfo);
+                return Ok($"User {userInfo.Email} authenticated successfully");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
