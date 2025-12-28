6ac2e3b1abad2b53a5002ac9521197a8ca9123ee
simorai <109289730+simorai@users.noreply.github.com>
2025-12-28 23:07:04 +0000

    Add JWT support and authentication models
    Added JWT configuration to `appsettings.json` with `Key`, `Issuer`, and `Audience` for token-based authentication.
    
    Introduced `LoginModel` and `RegisterModel` classes with validation attributes to handle user login and registration inputs.
    
    Added `UserToken` class to represent JWT tokens and their expiration details.

---
+++ CleanArchMvc.API/Models/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.API.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
