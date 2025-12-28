2d8da1b84d801f9a4ecd137fc6e5e4acd82cb322
simorai <109289730+simorai@users.noreply.github.com>
2025-12-28 22:38:07 +0000

    Add ProductsController and update CategoriesController
    Updated the `CategoriesController` to modify the `[HttpPut]`
    attribute, explicitly including an `"id"` parameter in the route.
    
    Added a new `ProductsController` to handle CRUD operations for
    products. Implemented methods for retrieving all products,
    retrieving a product by ID, creating, updating, and deleting
    products. The controller uses `IProductService` for business
    logic, adhering to Clean Architecture principles.

---
+++ CleanArchMvc.API/Controllers/ProductsController.cs
@@ -0,0 +1,79 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var products = await _productService.GetProducts();
+            if (products == null)
+            {
+                return NotFound("Products not found");
+            }
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int? id)
+        {
+            var product = await _productService.GetById(id);
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] ProductDTO productDto)
+        {
+            if (productDto == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+            await _productService.Add(productDto);
+            return new CreatedAtRouteResult("GetProduct", new { id = productDto.Id }, productDto);
+        }
+
+        [HttpPut("id")]
+        public async Task<ActionResult> Put(int id, [FromBody] ProductDTO productDto)
+        {
+            if (id != productDto.Id)
+            {
+                return BadRequest("Product ID mismatch.");
+            }
+
+            if (productDto == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+            await _productService.Update(productDto);
+            return Ok(productDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int? id)
+        {
+            var productDto = await _productService.GetById(id);
+            if (productDto == null)
+            {
+                return NotFound("Product not found");
+            }
+            await _productService.Remove(id);
+            return Ok(productDto);
+        }
+    }
+}
